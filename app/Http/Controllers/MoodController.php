d5782e09a3dfc898c785b97596f8f80b53e14b7f
Oliwer22 <oliwer.w2209@gmail.com>
2024-02-23 22:26:09 +0100

    Update 1
    Begin of the MoodMeter Challenge Laravel/Node.js

---
+++ app/Http/Controllers/MoodController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MoodController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function store(Request $request)
+    {
+        // Handle storing mood data 
+
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mood Meter</title>
+    <!-- Add your CSS and JS references here -->
+</head>
+<body>
+    <h1>Moodewew Meter</h1>
+    <!-- Your mood meter UI goes here -->
+    <script src="http://localhost:3000/index.js"></script>
+</body>
+</html>
